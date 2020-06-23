5f9fbf16c0eee98a89528fd94e7b6811cc57324c
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-23 13:47:56 -0300

    updated projects view

--- src/views/projects.view.jsx
+++ src/views/projects.view.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 // components
 import TopBarComponent from '../components/topBar.component';
 import Footer from '../components/footer.component';
 import ProjectBox from '../components/projectBox.component';
+import Button from '../components/env/button.component';
 
 // services
 import { getAll } from '../services/project.service';
 
 const Grid = styled.div`
-  display: grid;
+  display: ${props => props.display};
   width: 80vw;
   margin-left: auto;
   margin-right: auto;
@@ -26,27 +28,51 @@ const Title = styled.h1`
   padding-top: 35px;
 `;
 
+const Text = styled.h1`
+  padding-bottom: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
 const Projects = ({ logged, userData }) => {
   const [projects, setProjects] = useState(false);
+  const [display, setDisplay] = useState('');
 
   useEffect(() => {
-    getAll('boi12345').then(data => setProjects(data));
+    if (localStorage.getItem('logged') === 'true') {
+      getAll(localStorage.getItem('username'))
+        .then(data => setProjects(data));
+      setDisplay('grid');
+    }
   }, []);
 
   return (
     <div className="projects">
       <TopBarComponent logged={logged} userData={userData} />
       <Title>Meus Projetos</Title>
-      <Grid>
+      <Grid display={display}>
         {
           (() => {
-            if (projects) {
-              const project = projects.map(project_ => <ProjectBox project={project_} />);
+            if (localStorage.getItem('logged') === 'true') {
+              if (projects) {
+                const project = projects.map(project_ => <ProjectBox project={project_} />);
 
-              return project;
+                return project;
+              }
+
+              return;
             }
 
-            return;
+            return (
+              <div style={{ textAlign: 'center' }}>
+                <Text>Você precisa estar logado para ver seus projetos...</Text>
+                <StyledLink to="/login">
+                  <Button width="200px" text="Fazer login" callback={() => ''} />
+                </StyledLink>
+              </div>
+            );
           })()
         }
       </Grid>
