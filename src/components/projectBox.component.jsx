9a12c62e7eb196ddf51746bf27d6753ef84175e1
danillu <48914278+Raisess@users.noreply.github.com>
2020-06-23 11:21:16 -0300

    updated ProjectBox and Project view

--- src/components/projectBox.component.jsx
+++ src/components/projectBox.component.jsx
@@ -6,28 +6,35 @@ const Box = styled.div`
   height: 180px;
   background-color: #aaa;
   cursor: pointer;
+  border-radius: 5px;
+  box-shadow: 0 0 3px #333;
 `;
 
 const Img = styled.img`
   width: 100%;
-  height: 65%;
+  height: 70%;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
 `;
 
 const SubBox = styled.div`
-  background-color: ${props => props.bg || 'red'};
+  background-color: ${props => props.bg};
   width: 100%;
-  height: 40%;
+  height: 30%;
   color: #eee;
-  margin-top: -5px;
+  margin-top: -3.7px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
 `;
 
 const Username = styled.span`
   font-size: 12px;
-  padding-right: 10px;
+  padding-left: 10px;
+  padding-top: 5px;
 `;
 
 const ProjectName = styled.h3`
-  padding-right: 10px;
+  padding-left: 10px;
 `;
 
 const ProjectBox = ({ project }) => {
--- src/views/projects.view.jsx
+++ src/views/projects.view.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 // components
 import TopBarComponent from '../components/topBar.component';
 import Footer from '../components/footer.component';
+import ProjectBox from '../components/projectBox.component';
 
 // services
 import { getAll } from '../services/project.service';
 
+const Grid = styled.div`
+  display: grid;
+  width: 80vw;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 60px;
+  grid-gap: 2em;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
 const Projects = ({ logged, userData }) => {
-  getAll('boi12345').then(data => console.log(data));
+  const [projects, setProjects] = useState(false);
+
+  useEffect(() => {
+    getAll('boi12345').then(data => setProjects(data));
+  }, []);
 
   return (
     <div className="projects">
       <TopBarComponent logged={logged} userData={userData} />
       <h1>Projects</h1>
+      <Grid>
+        {
+          (() => {
+            if (projects) {
+              const project = projects.map(project_ => <ProjectBox project={project_} />);
+
+              return project;
+            }
+
+            return;
+          })()
+        }
+      </Grid>
       <Footer />
     </div>
   );
